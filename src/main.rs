17855c22ec455f1dd4d4af3c3479d103991f3960
0xlax <harshu8310@gmail.com>
2022-01-31 01:09:09 +0530

    delete tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,46 @@
-use std::env::args();
+#![allow(unused)]
+use clap::Parser;
+use std::env::args;
+use std::io::{ BufReader,Read };
+use std::path::PathBuf;
+use std::fs::File;
+
+
+
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: PathBuf,
+}
 
 
 fn main() {
-    let pattern = args().nth(1).expect("No Pattern Given");
-    let path = args().nth(2).expect("No path Given");
-    
+    let args = Cli::parse();
+
+    let f = File::open(&args.path).expect("Could not read file");
+
+    let mut buf_reader = BufReader::new(f);
+
+    let mut contents = String::new();
+
+    buf_reader.read_to_string(&mut contents).expect("_BUFFER_ERROR_");
+
+
+
+    fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+        for line in contents.lines() {
+            if line.contains(&args.pattern) {
+                println!("{}", line);
+            }
+        }
+    }
+
+
+
 }
+
+
+
+
